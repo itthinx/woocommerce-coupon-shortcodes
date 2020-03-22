01b6cff4e2ef78d16e77612cb1d142198302b060
itthinx <itthinx@gmail.com>
2020-03-22 21:43:18 +0100

    2020

--- woocommerce-coupon-shortcodes.php
+++ woocommerce-coupon-shortcodes.php
@@ -2,7 +2,7 @@
 /**
  * woocommerce-coupon-shortcodes.php
  *
- * Copyright (c) 2013-2019 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2013-2020 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
